f49b66367d0156590bdaa34331b519e2a2dbd519
Kendall Dyke <kendalldyke14@gmail.com>
2018-04-10 09:59:52 -0400

    lec ex

--- model.py
+++ model.py
@@ -6,8 +6,29 @@ BB_FILE_NAME = 'umbball.csv'
 bb_seasons = []
 
 def init_bball(csv_file_name):
-    pass
+    with open(csv_file_name) as bballCsvFile:
+        bballData = csv.reader(bballCsvFile)
+        next(bballData) #throw away headers
+        next(bballData) #throw away headers
+        global bb_seasons
+        bb_seasons = [] #reset, start clean
+        for row in bballData:
+            row[3] = int(row[3])
+            row[4] = int(row[4])
+            row[5] = float(row[5])
+            bb_seasons.append(row)
+
 
 def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
